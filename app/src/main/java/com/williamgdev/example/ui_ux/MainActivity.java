fbefaca517d5d300bc3efcdee32470fe60a817fd
AndroidStorm <williamg.dev@gmail.com>
2018-02-26 15:37:16 -0800

    DataBinding User model

--- app/src/main/java/com/williamgdev/example/ui_ux/MainActivity.java
+++ app/src/main/java/com/williamgdev/example/ui_ux/MainActivity.java
@@ -1,13 +1,20 @@
 package com.williamgdev.example.ui_ux;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.williamgdev.example.ui_ux.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User user = new User();
+        user.setFullName("Will GT");
+        user.setName("Will");
+        binding.setUser(user);
     }
 }
---
+++ app/src/main/java/com/williamgdev/example/ui_ux/User.java
@@ -0,0 +1,26 @@
+package com.williamgdev.example.ui_ux;
+
+/**
+ * Created by willimail on 2/26/18.
+ */
+
+public class User {
+    String name;
+    String fullName;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
