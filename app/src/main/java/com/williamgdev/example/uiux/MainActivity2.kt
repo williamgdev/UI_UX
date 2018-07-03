cc362b52e06742796b48498ba8a19a05520b8a3d
AndroidStorm <williamg.dev@gmail.com>
2018-07-02 23:02:13 -0700

    Fixed layouts

--- app/src/main/java/com/williamgdev/example/uiux/MainActivity2.kt
+++ app/src/main/java/com/williamgdev/example/uiux/MainActivity2.kt
@@ -3,7 +3,7 @@ package com.williamgdev.example.ui_ux
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
